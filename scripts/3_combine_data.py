45b3dde36c96cb7395933ed59ca37dc7b025e9ea
maximusquant <ivanmatuskin01@gmail.com>
2026-07-27 14:37:36 +0300

    Update 3_combine_data.py

--- scripts/3_combine_data.py
+++ scripts/3_combine_data.py
@@ -4,18 +4,20 @@ import os
 # ============================================
 # 1. ЗАГРУЗКА ДАННЫХ
 # ============================================
+
 # Загружаем df_final (цены всех инструментов)
 df_final = pd.read_csv('data/df_final.csv', parse_dates=['DATE_MIX'])
 df_final.set_index('DATE_MIX', inplace=True)
 
-# Загружаем OI данные
-df_oi = pd.read_csv('MIX_OI_daily_CORRECT.csv', parse_dates=['date'])
+# Загружаем OI данные (из data/)
+df_oi = pd.read_csv('data/MIX_OI_daily_CORRECT.csv', parse_dates=['date'])
 df_oi.set_index('date', inplace=True)
 
 # ============================================
 # 2. ОБЪЕДИНЕНИЕ
 # ============================================
 # Соединяем по дате
+df_oi.index.name = 'DATE_MIX'   # выравниваем имя индекса перед объединением
 df_combined = pd.merge(df_final, df_oi, left_index=True, right_index=True, how='left')
 
 # Заполняем пропуски в OI (если есть)
